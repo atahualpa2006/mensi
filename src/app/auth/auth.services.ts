f21053d5391e182ea874735edcb0274b7755440d
Diego Mensi <mensidiego@gmail.com>
2023-08-04 00:20:40 -0300

    login con usuarios

--- src/app/auth/auth.services.ts
+++ src/app/auth/auth.services.ts
@@ -4,6 +4,8 @@ import { User } from "../dashboard/pages/users/models";
 import { BehaviorSubject, Observable, map, take } from "rxjs";
 import { NotifierService } from "../core/services/notifier.service";
 import { Router } from "@angular/router";
+import { HttpClient } from "@angular/common/http";
+import { RepositionScrollStrategy } from "@angular/cdk/overlay";
 
 
 
@@ -13,7 +15,10 @@ export class  AuthService {
     private _authUser$ = new BehaviorSubject <User | null> (null);
     public authUser$ = this._authUser$.asObservable();
 
-    constructor (private notifier: NotifierService, private router: Router) {}
+    constructor (
+        private notifier: NotifierService, 
+        private router: Router,
+        private httpClient:HttpClient ) {}
 
         isAuthenticated() : Observable <boolean> {
             return this.authUser$.pipe(
@@ -25,22 +30,47 @@ export class  AuthService {
 
 
         login (payLoad: loginPayload): void {
-        const MOCK_USER: User = {
-        id: 50,
-        name:'Mockname',
-        surname:'Mocksurname',
-        email:'mock@mail.com',
-        password:'1',
-        }
 
-        if (payLoad.email === MOCK_USER.email && payLoad.password === MOCK_USER.password) {
+// metodo http clien con usuario registrado
 
-            this._authUser$.next(MOCK_USER);
-            this.router.navigate(['/dashboard/home']);
-        } else {
-            this.notifier.showError('Email o contrasena invalida');
-            this._authUser$.next(null);
-        }
+            this.httpClient.get <User[]> ('http://localhost:3000/users', {
+                params: {
+                    email: payLoad.email || '',
+                    password: payLoad.password ||''
+
+                }
+            }).subscribe({
+                next:(response) => {
+
+                    if (response.length) {
+                        // login valido
+                        this._authUser$.next(response[0]);
+                        this.router.navigate(['/dashboard/home']);
+                    }else{
+                    //   login invalido  
+                    this.notifier.showError('Email o contrasena invalida');
+                    this._authUser$.next(null);
+                    
+                    }
+                }
+            })
+
+        // const MOCK_USER: User = {
+        // id: 50,
+        // name:'Mockname',
+        // surname:'Mocksurname',
+        // email:'mock@mail.com',
+        // password:'1',
+        // }
+
+        // if (payLoad.email === MOCK_USER.email && payLoad.password === MOCK_USER.password) {
+
+        //     this._authUser$.next(MOCK_USER);
+        //     this.router.navigate(['/dashboard/home']);
+        // } else {
+        //     this.notifier.showError('Email o contrasena invalida');
+        //     this._authUser$.next(null);
+        // }
 
     }    
 
